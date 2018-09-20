6f4da0f6c3b15b17122095faf77c4b2c5eb8155b
h-kawamine <kawamine.hideto@zp.mitsubishielectric.co.jp>
2018-09-20 17:56:13 +0900

    Book_2.3.2 NewAdd

---
+++ Mywork/AllTests.cpp
@@ -0,0 +1,16 @@
+/*
+ * AllTests.cpp
+ *
+ *  Created on: 2018/09/20
+ *      Author: P18886
+ */
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int argc, char **argv) {
+	return RUN_ALL_TESTS(argc,argv);
+	//return CommandLineTestRunner::RunAllTests(argc, argv);
+}
+
+
+
+
